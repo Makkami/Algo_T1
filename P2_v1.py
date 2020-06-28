a06ddf8fad400f41aba4b92a4500ae075e5cb95f
KathiyS <66759905+KathiyS@users.noreply.github.com>
2020-06-28 18:25:41 -0400

    Update P2_v1.py

--- P2_v1.py
+++ P2_v1.py
@@ -1,3 +1,5 @@
+#Definicion de funcion Join de Pregunta 1,
+#recibiendo por parametros las entradas requeridas.
 def Join(N, M, atr_R, atr_S, tablaR, tablaS):
     atr_iguales = list(set(atr_R) & set(atr_S))
     atr_iguales.sort()
@@ -14,6 +16,7 @@ def Join(N, M, atr_R, atr_S, tablaR, tablaS):
 
     for filaR in tablaR:
         for filaS in tablaS:
+
             c = 0
             for i in range(L):
                 if filaR[posAtrR[i]] == filaS[posAtrS[i]]:
@@ -31,25 +34,35 @@ def Join(N, M, atr_R, atr_S, tablaR, tablaS):
                 ls_tuplas.sort()
                 filaT = [tupla[1] for tupla in ls_tuplas]
                 tablaT.append(filaT)
+
     return tablaT
+        
+'''Comienzo programa Pregunta 2:'''
 
+#Lista que almacena los inputs
 Tabla = list()
-cont_arcos = 0
-conjunto = set()
+#Lista que almacena los triangulos encontrados
+matches = list() 
 
+#Solicitud de inputs
 arco = input("Ingrese arco: ")
 
 while arco!='EOF':
     arcos = list(map(int, arco.split(" ")))
     Tabla.append(arcos)
-    cont_arcos += 1
     arco = input("Ingrese arco: ")
 
-tablaT = Join(2, 2, [0,1], [1,2], Tabla, Tabla)
+if len(Tabla)!=0:
+    #Uso de la funcion Join con los inputs
+    tablaT = Join(2, 2, [0,1], [1,2], Tabla, Tabla)
+    #Recorremos solucion de lo que entrega Join
+    for a, b, c in tablaT:
+        #Ya que existe a->b y b->c, buscamos el tercer arco dirigido necesario c->a.
+        #Si lo encuentra lo agrega a lista matches.
+        if ([b,c,a] in tablaT) or ([c,a,b] in tablaT):
+            matches.append([a,b,c])
 
-for a, b, c in tablaT:
-    if ([b,c,a] in tablaT) or ([c,a,b] in tablaT):
-        lista = [a, b, c]
-        lista.sort()
-        conjunto.add(tuple(lista))
-print (len(conjunto))
+#Entrega del resultado
+print (int(len(matches)/3))
+#Se divide en 3 puesto que al hacer el Join, cada triangulo tiene asociado
+#sus tres combinaciones posibles.
